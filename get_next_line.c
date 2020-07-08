65d565960a3db3b14aea9cb9bb18bcd9ea917d78
ngenadie <ngenadie@student.42.fr>
2020-07-08 17:14:16 +0200

    gnl update

--- get_next_line.c
+++ get_next_line.c
@@ -37,17 +37,38 @@ static char *ft_realloc(char *str, int size)
 	return (new_str);
 }
 
+int ft_len_before_nl(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
 int get_next_line(int fd, char **s)
 {
 	char buff[BUFF_SIZE + 1];
 	(void)s;
 	int ret;
 	int row;
+	int i;
 	static char *str;
-	
-	row = BUFF_SIZE;
-	if((str = malloc(BUFF_SIZE)) == NULL)
+	static int call_nb = 1;
+	static int is_filled = 0;
+
+	if (BUFF_SIZE <= 0)
 		return (-1);
+	i = 0;
+	row = BUFF_SIZE;
+	if (is_filled == 0)
+		if((str = malloc(BUFF_SIZE)) == NULL)
+			return (-1);
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		row += ret;
@@ -55,10 +76,18 @@ int get_next_line(int fd, char **s)
 			return (-1);
 		buff[ret] = '\0';
 		ft_strcat(str, buff);
+		is_filled = 1;
 		if (ret < BUFF_SIZE)
 			break;
 	}
-	printf("%s", str);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+	{
+		write(1, str, i + 1);
+		str += i + 1;
+		return (1);
+	}
 	return (0);
 }
 
@@ -66,15 +95,11 @@ int main(int argc, char **argv)
 {
 	char *s;
 	int fd;
+	int i;
 
 	if((fd = open("PAGE", O_RDONLY)) == -1)
 		return (-1);
-	get_next_line(fd, &s);
-	get_next_line(fd, &s);
-//	while (get_next_line(fd, &s))
-//	{
-//		printf("%s\n", s);
-//		free(s);
-//	}
+	while(get_next_line(fd, &s) != 0)
+		i++;
 	return (0);
 }

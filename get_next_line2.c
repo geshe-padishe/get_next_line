5dd71ed44a18934aba1140c887ce6b09c8f3608c
Niki NIKOLOV <ngenadie@e1r6p12.42.fr>
2020-08-02 18:23:38 +0200

    feawrae

--- get_next_line2.c
+++ get_next_line2.c
@@ -1,21 +1,39 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+#include <unistd.h>
 
-int ft_nl_index(str)
+int ft_nl_index(char *str)
 {
 	int i;
 
-	i = -1;
-	while (str[++i])
-		if (str[i] == '\n')
-			return (i);
+	if (str == NULL)
+		str = malloc(BUFF_SIZE + 1);
+	else
+	{
+		i = -1;
+		while (str[++i])
+			if (str[i] == '\n')
+				return (i);
+	}
 	return (0);
 }
 
-char *ft_send_&_clean(char *s, char *str, int nl_index)
+int ft_strlen(char *str)
 {
 	int i;
-	char *s;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *ft_send_clean(char *s, char *str, int nl_index)
+{
+	int i;
+	char *new_str;
 
 	s = malloc(nl_index + 1);
 	new_str = malloc(ft_strlen(str) - nl_index + 1);
@@ -33,17 +51,8 @@ char *ft_send_&_clean(char *s, char *str, int nl_index)
 	}
 	new_str[i] = '\0';
 	free(str);
+	str = NULL;
 	return (new_str);
-}
-
-int ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
 }
 
 char *ft_catfree(char *str, char *buff)
@@ -55,12 +64,20 @@ char *ft_catfree(char *str, char *buff)
 
 	i = 0;
 	j = 0;
-	len = ft_strlen(str) + ft_strlen(buff);
-	new_str = malloc(len + 1);
-	while (str[i])
+	if (str == NULL)
 	{
-		new_str[i] = str[i];
-		i++;
+		len = ft_strlen(buff);
+		new_str = malloc(len + 1);
+	}
+	else
+	{
+		len = ft_strlen(str) + ft_strlen(buff);
+		new_str = malloc(len + 1);
+		while (str[i])
+		{
+			new_str[i] = str[i];
+			i++;
+		}
 	}
 	while (buff[j])
 	{
@@ -69,36 +86,46 @@ char *ft_catfree(char *str, char *buff)
 		i++;
 	}
 	new_str[i] = '\0';
-	free(str);
-	free(buff);
+	if (str != NULL)
+		free(str);
+	str = NULL;
 	return (new_str);
 }
 
 int get_next_line(int fd, char **line)
 {
 	int nl_index;
-	static char *str = {'\0'};
 	char buff[BUFF_SIZE + 1];
+	static char *str;
 
 	if ((nl_index = ft_nl_index(str)) > 0)
-		str = ft_send_&_clean(line[0], str, nl_index);
+		str = ft_send_clean(line[0], str, nl_index);
 	else
+	{
 		while (read(fd, buff, BUFF_SIZE) > 0 && (nl_index = ft_nl_index(str)) == 0)
 			str = ft_catfree(str, buff);
-	if (nl_index == 0)
-	{
-		str = ft_send_&_clean(line[0], str, ft_strlen(str));
-		free(str);
+		if (nl_index == 0)
+		{
+			str = ft_send_clean(line[0], str, ft_strlen(str));
+			free(str);
+			str = NULL;
+			return (0);
+		}
+		else
+			str = ft_send_clean(line[0], str, nl_index);
 	}
-	else
-		str = ft_send_&_clean(line[0], str, nl_index);
+	return (1);
 }
 
 int main()
 {
+	char *s;
+	int fd;
+	int i;
+
 	if ((fd = open("PAGE", O_RDONLY)) == -1)
 		return (-1);
 	while (get_next_line(fd, &s) != 0)
-		i++;
+		printf("%s\n", s);
 	return (0);
 }
